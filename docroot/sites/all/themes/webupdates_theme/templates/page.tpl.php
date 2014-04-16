537a3b75d2d9d67964648ad0eab09a3cd12de4ef
Aaron Froehlich <aaronfroehlich@gmail.com>
2014-04-16 12:48:46 -0400

    Adds prod-sync and updates styles for webupdates blog

--- docroot/sites/all/themes/webupdates_theme/templates/page.tpl.php
+++ docroot/sites/all/themes/webupdates_theme/templates/page.tpl.php
@@ -12,15 +12,9 @@
       <?php if ($site_name || $site_slogan): ?>
         <div id="name-and-slogan">
           <?php if ($site_name): ?>
-            <?php if ($title): ?>
-              <div id="site-name"><strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong></div>
-            <?php else: /* Use h1 when the content title is empty */ ?>
-              <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </h1>
-            <?php endif; ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
           <?php endif; ?>
 
           <?php if ($site_slogan): ?>
